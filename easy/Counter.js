174dbf247d6a5fe9a9fe79b023b9d98006a60a15
Lea-Brown <leabrown198x@gmail.com>
2024-06-29 18:12:13 +0100

    description change

--- easy/Counter.js
+++ easy/Counter.js
@@ -1,6 +1,6 @@
 /* 2620. Counter
 
-https://leetcode.com/problems/counter/description/?envType=study-plan-v2&envId=30-days-of-javascript
+https://leetcode.com/problems/counter/description/
 
 Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
 
