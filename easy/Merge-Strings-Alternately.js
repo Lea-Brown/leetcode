d6c0a99aea9a6f4f1cf14c82353939c34585fb26
Lea-Brown <leabrown198x@gmail.com>
2024-10-01 13:29:53 +0100

    leetcode 1/10

--- easy/Merge-Strings-Alternately.js
+++ easy/Merge-Strings-Alternately.js
@@ -4,7 +4,7 @@ https://leetcode.com/problems/merge-strings-alternately/
 
 You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
 
-Return the merged string.
+Return the merged string. 
 
 Example 1:
 Input: word1 = "abc", word2 = "pqr"
