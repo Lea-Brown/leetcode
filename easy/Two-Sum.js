d7b8ae6e07f3d838078e11fed72f74c08f2640c4
Lea-Brown <leabrown198x@gmail.com>
2024-11-02 14:15:07 +0000

    update to code

--- easy/Two-Sum.js
+++ easy/Two-Sum.js
@@ -1,6 +1,6 @@
 /* 1. Two Sum
 
-https://leetcode.com/problems/two-sum/description/
+https://leetcode.com/problems/two-sum/
 
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
@@ -9,21 +9,19 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.
 
 Example 1:
-
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-Example 2:
 
+Example 2:
 Input: nums = [3,2,4], target = 6
 Output: [1,2]
-Example 3:
 
+Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
  
 Constraints:
-
 2 <= nums.length <= 104
 -109 <= nums[i] <= 109
 -109 <= target <= 109
@@ -32,19 +30,16 @@ Only one valid answer exists.
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 */
 
+// chatGPT
 function twoSum(nums, target) {
   const numIndices = {};
-
   for (let i = 0; i < nums.length; i++) {
     const currentNum = nums[i];
     const complement = target - currentNum;
-
     if (complement in numIndices) {
       return [numIndices[complement], i];
     }
-
     numIndices[currentNum] = i;
   }
-
   return [];
 }
