4c01d9b8788d345f9d8888526a282d8856a10e24
Lea-Brown <leabrown198x@gmail.com>
2024-11-03 15:41:12 +0000

    working on hash maps

--- easy/Ransom-Note.js
+++ easy/Ransom-Note.js
@@ -28,16 +28,31 @@ ransomNote and magazine consist of lowercase English letters.
 
 var canConstruct = function (ransomNote, magazine) {
   let result = [];
-  let rn = ransomNote.split('').sort();
-  let m = magazine.split('').sort();
+  let rn = ransomNote.split("").sort();
+  let m = magazine.split("").sort();
   for (let i = 0; i < ransomNote.length; i++) {
     if (m.includes(rn[i])) {
-      result.push(m.find(x => x == rn[i]));
+      result.push(m.find((x) => x == rn[i]));
       m.splice(
-        m.findIndex(x => x == rn[i]),
+        m.findIndex((x) => x == rn[i]),
         1
       );
     }
   }
   return result.length == ransomNote.length;
 };
+
+// working on hash maps
+function canConstruct(ransomNote, magazine) {
+  const obj = {};
+  const length = Math.max(ransomNote.length, magazine.length);
+  for (let i = 0; i < length; i++) {
+    obj[ransomNote[i]] = (obj[ransomNote[i]] || 0) + 1;
+    obj[magazine[i]] = (obj[magazine[i]] || 0) - 1;
+  }
+  const set = new Set(ransomNote);
+  for (const letter of set) {
+    if (obj[letter] > 0) return false;
+  }
+  return true;
+}
