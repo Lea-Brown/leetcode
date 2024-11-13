bac34002970df5fd1f81f2485ab4fbbd9533cf0c
Lea <leabrown198x@gmail.com>
2024-11-13 22:56:43 +0000

    trying some typescript

---
+++ easy/Fibonacci-Number.ts
@@ -0,0 +1,54 @@
+/* 509. Fibonacci Number
+
+https://leetcode.com/problems/fibonacci-number/
+
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+Example 1:
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+Example 2:
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+Example 3:
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+ 
+Constraints:
+0 <= n <= 30
+*/
+
+let doneFibs: { [key: string]: number } = {};
+function fib(n: number): number {
+  if (n < 2) {
+    return n;
+  } else if (doneFibs[n]) {
+    return doneFibs[n];
+  } else {
+    const newFib = fib(n - 1) + fib(n - 2);
+    doneFibs[n] = newFib;
+    return newFib;
+  }
+}
+
+// someone elses code. Need to learn
+function fib2(n: number, seen = {}): number {
+  //seem = {} is interesting
+  if (n <= 0) return 0;
+  if (n == 1 || n == 2) return 1;
+  if (seen[n]) {
+    return seen[n];
+  }
+  let result = fib2(n - 1, seen) + fib2(n - 2, seen);
+  seen[n] = result;
+  return result;
+}
