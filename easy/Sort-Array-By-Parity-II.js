7a5a8b4e9713b1e69f8be6973b8e49f76394d2c1
Lea <leabrown198x@gmail.com>
2025-01-19 16:09:21 +0000

    leetcode 19/1

---
+++ easy/Sort-Array-By-Parity-II.js
@@ -0,0 +1,40 @@
+/* 922. Sort Array By Parity II
+
+https://leetcode.com/problems/sort-array-by-parity-ii/
+
+Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+
+Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+
+Return any answer array that satisfies this condition.
+
+Example 1:
+Input: nums = [4,2,5,7]
+Output: [4,5,2,7]
+Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+
+Example 2:
+Input: nums = [2,3]
+Output: [2,3]
+ 
+Constraints:
+2 <= nums.length <= 2 * 104
+nums.length is even.
+Half of the integers in nums are even.
+0 <= nums[i] <= 1000
+ 
+Follow Up: Could you solve it in-place?
+*/
+
+function sortArrayByParityII(nums) {
+  let odd = [];
+  let even = [];
+  for (const each of nums) {
+    each % 2 === 0 ? even.push(each) : odd.push(each);
+  }
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    i % 2 === 0 ? result.push(even.pop()) : result.push(odd.pop());
+  }
+  return result;
+}
