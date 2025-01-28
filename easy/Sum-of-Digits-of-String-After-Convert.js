a9d8fdd5b665e1fe237b39cfc7b26b4700852c3c
Lea <leabrown198x@gmail.com>
2025-01-28 20:16:47 +0000

    leetcode 28/1

---
+++ easy/Sum-of-Digits-of-String-After-Convert.js
@@ -0,0 +1,87 @@
+/* 1945. Sum of Digits of String After Convert
+
+https://leetcode.com/problems/sum-of-digits-of-string-after-convert/
+
+You are given a string s consisting of lowercase English letters, and an integer k. Your task is to convert the string into an integer by a special process, and then transform it by summing its digits repeatedly k times. More specifically, perform the following steps:
+
+Convert s into an integer by replacing each letter with its position in the alphabet (i.e. replace 'a' with 1, 'b' with 2, ..., 'z' with 26).
+Transform the integer by replacing it with the sum of its digits.
+Repeat the transform operation (step 2) k times in total.
+For example, if s = "zbax" and k = 2, then the resulting integer would be 8 by the following operations:
+
+Convert: "zbax" ➝ "(26)(2)(1)(24)" ➝ "262124" ➝ 262124
+Transform #1: 262124 ➝ 2 + 6 + 2 + 1 + 2 + 4 ➝ 17
+Transform #2: 17 ➝ 1 + 7 ➝ 8
+Return the resulting integer after performing the operations described above.
+
+Example 1:
+Input: s = "iiii", k = 1
+Output: 36
+Explanation:
+The operations are as follows:
+- Convert: "iiii" ➝ "(9)(9)(9)(9)" ➝ "9999" ➝ 9999
+- Transform #1: 9999 ➝ 9 + 9 + 9 + 9 ➝ 36
+Thus the resulting integer is 36.
+
+Example 2:
+Input: s = "leetcode", k = 2
+Output: 6
+Explanation:
+The operations are as follows:
+- Convert: "leetcode" ➝ "(12)(5)(5)(20)(3)(15)(4)(5)" ➝ "12552031545" ➝ 12552031545
+- Transform #1: 12552031545 ➝ 1 + 2 + 5 + 5 + 2 + 0 + 3 + 1 + 5 + 4 + 5 ➝ 33
+- Transform #2: 33 ➝ 3 + 3 ➝ 6
+Thus the resulting integer is 6.
+
+Example 3:
+Input: s = "zbax", k = 2
+Output: 8
+
+Constraints:
+1 <= s.length <= 100
+1 <= k <= 10
+s consists of lowercase English letters.
+*/
+
+function getLucky(s, k) {
+  const alphabetMap = {
+    a: "1",
+    b: "2",
+    c: "3",
+    d: "4",
+    e: "5",
+    f: "6",
+    g: "7",
+    h: "8",
+    i: "9",
+    j: "10",
+    k: "11",
+    l: "12",
+    m: "13",
+    n: "14",
+    o: "15",
+    p: "16",
+    q: "17",
+    r: "18",
+    s: "19",
+    t: "20",
+    u: "21",
+    v: "22",
+    w: "23",
+    x: "24",
+    y: "25",
+    z: "26",
+  };
+  let count = "";
+  for (const each of s) {
+    count += alphabetMap[each];
+  }
+  console.log(count);
+  for (let i = 0; i < k; i++) {
+    count = count
+      .toString()
+      .split("")
+      .reduce((acc, add) => +acc + +add, 0);
+  }
+  return count;
+}
